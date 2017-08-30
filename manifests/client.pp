5034ac37b4b5c16993a8510881ed78db1c5d67c6
Ewoud Kohl van Wijngaarden <github@kohlvanwijngaarden.nl>
2017-08-30 15:38:53 +0200

    Refactor qpid::client (#74)
    Splitting into several classes is overkill if they only contain one
    resource each. It also allows the template to use local scope. For more
    certainty Puppet 4 types are used.

--- manifests/client.pp
+++ manifests/client.pp
@@ -3,20 +3,26 @@
 # Handles Qpid client package installations and configuration
 #
 class qpid::client(
-  $version                 = $qpid::client::params::version,
-  $config_file             = $qpid::client::params::config_file,
-  $log_level               = $qpid::client::params::log_level,
-  $ssl                     = $qpid::client::params::ssl,
-  $ssl_port                = $qpid::client::params::ssl_port,
-  $ssl_cert_db             = $qpid::client::params::ssl_cert_db,
-  $ssl_cert_password_file  = $qpid::client::params::ssl_cert_password_file,
-  $ssl_cert_name           = $qpid::client::params::ssl_cert_name,
-  $client_packages         = $qpid::client::params::client_packages,
+  String $version = $qpid::client::params::version,
+  Stdlib::Absolutepath $config_file = $qpid::client::params::config_file,
+  String $log_level = $qpid::client::params::log_level,
+  Boolean $ssl = $qpid::client::params::ssl,
+  Integer[0, 65535] $ssl_port  = $qpid::client::params::ssl_port,
+  Optional[Stdlib::Absolutepath] $ssl_cert_db = $qpid::client::params::ssl_cert_db,
+  Optional[Stdlib::Absolutepath] $ssl_cert_password_file = $qpid::client::params::ssl_cert_password_file,
+  Optional[String] $ssl_cert_name = $qpid::client::params::ssl_cert_name,
+  Array[String] $client_packages = $qpid::client::params::client_packages,
 ) inherits qpid::client::params {
 
-  include ::qpid::client::install
-  include ::qpid::client::config
-
-  Class['qpid::client::install'] ~> Class['qpid::client::config']
+  package { $client_packages:
+    ensure => $version,
+  } ->
+  file { $config_file:
+    ensure  => file,
+    content => template('qpid/qpidc.conf.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+  }
 
 }
