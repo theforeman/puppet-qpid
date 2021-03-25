f62dcc13fad4e5f4fb24d42fbbb505c6665523fe
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2021-03-25 18:40:47 +0100

    Remove duplicate relations
    The qpidd service relation is already set in init.pp. The direct package
    resource is replaced with the class that encapsulates it.

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,18 +2,17 @@
 #
 # @api private
 class qpid::install {
-
   include qpid::tools
 
   package { $qpid::server_packages:
     ensure => $qpid::version,
-    before => [Service['qpidd'],Package['qpid-tools']],
+    before => Class['qpid::tools'],
   }
 
   if $qpid::server_store {
     package { $qpid::server_store_package:
       ensure => $qpid::version,
-      before => [Service['qpidd'],Package['qpid-tools']],
+      before => Class['qpid::tools'],
     }
   }
 }
