127566ddfa997a5e15ef5d80c89e38178a0c1dfe
Eric D. Helms <ericdhelms@gmail.com>
2021-03-30 08:07:09 -0400

    Add types to router.pp

--- manifests/router/params.pp
+++ manifests/router/params.pp
@@ -2,12 +2,6 @@
 #
 # @api private
 class qpid::router::params {
-  $config_file         = '/etc/qpid-dispatch/qdrouterd.conf'
   $router_id           = $facts['networking']['fqdn']
-  $router_mode         = 'interior'
   $worker_threads      = $facts['processors']['count']
-  $router_packages     = ['qpid-dispatch-router']
-  $open_file_limit     = undef
-  $hello_max_age       = undef
-  $hello_interval      = undef
 }
