4f5198559878c8783abfb84e2d68d9e3f0003bb4
William Bradford Clark <wclark@redhat.com>
2021-04-05 17:24:21 -0400

    Refs #32037: Add ensure on qpid::tools class to allow removal of package

--- spec/acceptance/qpid_tool_spec.rb
+++ spec/acceptance/qpid_tool_spec.rb
@@ -14,4 +14,20 @@ describe 'qpid::tools' do
       it { is_expected.to be_installed }
     end
   end
+
+  context 'removing qpid-tools' do
+    let(:pp) do
+      <<-PUPPET
+      class { 'qpid::tools':
+        ensure => 'absent',
+      }
+      PUPPET
+    end
+
+    it_behaves_like 'a idempotent resource'
+
+    describe package('qpid-tools') do
+      it { is_expected.not_to be_installed }
+    end
+  end
 end
