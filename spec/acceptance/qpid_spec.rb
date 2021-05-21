5def21a70d178a6e472954bc7aa03449e6b96d93
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2021-05-21 19:04:43 +0200

    Switch to voxpupuli-acceptance-1.0

--- spec/acceptance/qpid_spec.rb
+++ spec/acceptance/qpid_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper_acceptance'
 
 describe 'qpid', :order => :defined do
   context 'with default parameters' do
-    let(:pp) do
-      <<-PUPPET
-      include qpid
-      PUPPET
+    it_behaves_like 'an idempotent resource' do
+      let(:manifest) do
+        <<-PUPPET
+        include qpid
+        PUPPET
+      end
     end
-
-    it_behaves_like 'a idempotent resource'
 
     describe service('qpidd') do
       it { is_expected.to be_running }
@@ -29,15 +29,15 @@ describe 'qpid', :order => :defined do
   end
 
   context 'with ensure absent' do
-    let(:pp) do
-      <<-PUPPET
-      class { 'qpid':
-        ensure => 'absent',
-      }
-      PUPPET
+    it_behaves_like 'an idempotent resource' do
+      let(:manifest) do
+        <<-PUPPET
+        class { 'qpid':
+          ensure => 'absent',
+        }
+        PUPPET
+      end
     end
-
-    it_behaves_like 'a idempotent resource'
 
     describe service('qpidd') do
       it { is_expected.not_to be_running }
