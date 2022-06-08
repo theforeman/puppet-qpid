7e1eb33b826726283be69d202f7744ed4563dfc5
Eric D. Helms <ericdhelms@gmail.com>
2022-06-07 21:45:20 -0400

    Drop EL7 support

--- spec/setup_acceptance_node.pp
+++ spec/setup_acceptance_node.pp
@@ -1,18 +1,12 @@
-if $facts['os']['release']['major'] == '8' {
-  package { 'glibc-langpack-en':
-    ensure => installed,
-  }
+package { 'glibc-langpack-en':
+  ensure => installed,
+}
 
-  yumrepo { 'powertools':
-    enabled => true,
-  }
+yumrepo { 'powertools':
+  enabled => true,
+}
 
-  yumrepo { 'katello':
-    baseurl  => "http://yum.theforeman.org/katello/nightly/katello/el8/x86_64/",
-    gpgcheck => 0,
-  }
-} elsif $facts['os']['release']['major'] == '7' {
-  package { 'epel-release':
-    ensure => installed,
-  }
+yumrepo { 'katello':
+  baseurl  => "http://yum.theforeman.org/katello/nightly/katello/el8/x86_64/",
+  gpgcheck => 0,
 }
