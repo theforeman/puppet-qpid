549170bc99040e6359c059a05ca53bb7dc3432b9
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2023-11-07 15:44:24 +0100

    Use on_supported_os in bind_spec.rb
    Without this the facts are not set and Puppet can get upset about that.

--- spec/defines/bind_spec.rb
+++ spec/defines/bind_spec.rb
@@ -13,64 +13,69 @@ describe 'qpid::config::bind' do
       }
     end
 
-    it { is_expected.to compile.with_all_deps }
+    on_supported_os.each do |os, os_facts|
+      context "on #{os}" do
+        let(:facts) { os_facts }
+        it { is_expected.to compile.with_all_deps }
 
-    it do
-      is_expected.to contain_qpid__config_cmd('bind queue to exchange and filter messages that deal with *.*')
-        .with_command('bind event myqueue *.*')
-        .with_unless('exchanges event -r | grep *.*')
-        .with_hostname('localhost')
-        .with_port(nil)
-        .with_ssl_cert(nil)
-        .with_ssl_key(nil)
-    end
-  end
+        it do
+          is_expected.to contain_qpid__config_cmd('bind queue to exchange and filter messages that deal with *.*')
+            .with_command('bind event myqueue *.*')
+            .with_unless('exchanges event -r | grep *.*')
+            .with_hostname('localhost')
+            .with_port(nil)
+            .with_ssl_cert(nil)
+            .with_ssl_key(nil)
+        end
+      end
 
-  context 'with ssl_cert' do
-    let :params do
-      {
-        'exchange' => 'event',
-        'queue'    => 'myqueue',
-        'hostname' => 'myhost.example.com',
-        'port'     => 5671,
-        'ssl_cert' => '/path/to/cert.pem',
-        'ssl_key'  => '/path/to/key.pem',
-      }
-    end
+      context 'with ssl_cert' do
+        let :params do
+          {
+            'exchange' => 'event',
+            'queue'    => 'myqueue',
+            'hostname' => 'myhost.example.com',
+            'port'     => 5671,
+            'ssl_cert' => '/path/to/cert.pem',
+            'ssl_key'  => '/path/to/key.pem',
+          }
+        end
 
-    it { is_expected.to compile.with_all_deps }
+        it { is_expected.to compile.with_all_deps }
 
-    it do
-      is_expected.to contain_qpid__config_cmd('bind queue to exchange and filter messages that deal with *.*')
-        .with_command('bind event myqueue *.*')
-        .with_unless('exchanges event -r | grep *.*')
-        .with_hostname('myhost.example.com')
-        .with_port(5671)
-        .with_ssl_cert('/path/to/cert.pem')
-        .with_ssl_key('/path/to/key.pem')
-    end
-  end
+        it do
+          is_expected.to contain_qpid__config_cmd('bind queue to exchange and filter messages that deal with *.*')
+            .with_command('bind event myqueue *.*')
+            .with_unless('exchanges event -r | grep *.*')
+            .with_hostname('myhost.example.com')
+            .with_port(5671)
+            .with_ssl_cert('/path/to/cert.pem')
+            .with_ssl_key('/path/to/key.pem')
+        end
+      end
 
-  context 'with chaining' do
-    let :params do
-      {
-        'exchange' => 'event',
-        'queue' => 'myqueue',
-      }
-    end
+      context 'with chaining' do
+        let :params do
+          {
+            'exchange' => 'event',
+            'queue' => 'myqueue',
+          }
+        end
 
-    let :pre_condition do
-      <<-EOS
+        let :pre_condition do
+          <<-EOS
       qpid::config::exchange { 'event': }
       qpid::config::queue { 'myqueue': }
-      EOS
-    end
+          EOS
+        end
 
-    it { is_expected.to compile.with_all_deps }
+        it { is_expected.to compile.with_all_deps }
 
-    it do
-      is_expected.to contain_qpid__config_cmd('bind queue to exchange and filter messages that deal with *.*')
-        .that_requires(['Qpid::Config::Exchange[event]', 'Qpid::Config::Queue[myqueue]'])
+        it do
+          is_expected.to contain_qpid__config_cmd('bind queue to exchange and filter messages that deal with *.*')
+            .that_requires(['Qpid::Config::Exchange[event]', 'Qpid::Config::Queue[myqueue]'])
+        end
+      end
     end
   end
 end
